5fc546bd7cc247a379798f10c2a5d58c6ccca361
EricAngel1 <eric.angel9288@alumnos.udg.mx>
2025-08-24 15:39:02 -0600

    Agrege comentarios en cada bloque de codigo

--- While_motorola.s
+++ While_motorola.s
@@ -20,6 +20,8 @@ LC6:
 	.ascii "Multiplicacion: %d * %d = %d\12\0"                ; Formato printf mult ; Transferencia de Datos
 LC7:
 	.ascii "Division: %d / %d = %d\12\0"                      ; Formato printf div  ; Transferencia de Datos
+
+;Inicio del Main
 	.text
 	.globl	_main
 	.def	_main;	.scl	2;	.type	32;	.endef
@@ -34,22 +36,32 @@ LFB10:
 	andl	$-16, %esp                   ; Alinea stack en múltiplos de 16  ; Lógica
 	subl	$32, %esp                    ; Reserva 32 bytes para variables  ; Aritmética
 	call	___main                      ; Llama inicializador runtime C    ; Control de Flujo
+
+;Lectura de variable a
 	movl	$LC0, (%esp)                 ; Dirección de mensaje LC0         ; Transferencia de Datos
 	call	_puts                        ; puts("Ingresa el primer...")     ; Control de Flujo
 	leal	24(%esp), %eax               ; Dirección local de a             ; Transferencia de Datos
 	movl	%eax, 4(%esp)                ; Arg2 = &a                        ; Transferencia de Datos
 	movl	$LC1, (%esp)                 ; Arg1 = "%d"                      ; Transferencia de Datos
 	call	_scanf                       ; scanf("%d",&a)                   ; Control de Flujo
+
+;Lectura de la variable b
 	movl	$LC2, (%esp)                 ; Dirección mensaje LC2            ; Transferencia de Datos
 	call	_puts                        ; puts("Ingresa el segundo...")    ; Control de Flujo
 	leal	20(%esp), %eax               ; Dirección local de b             ; Transferencia de Datos
 	movl	%eax, 4(%esp)                ; Arg2 = &b                        ; Transferencia de Datos
 	movl	$LC1, (%esp)                 ; Arg1 = "%d"                      ; Transferencia de Datos
 	call	_scanf                       ; scanf("%d",&b)                   ; Control de Flujo
+
+;Mensaje de operaciones
 	movl	$LC3, (%esp)                 ; Dirección mensaje LC3            ; Transferencia de Datos
 	call	_puts                        ; puts("Calculando...")            ; Control de Flujo
+
+;Inicio del ciclo While
 	jmp	L2                             ; Salta a verificación del while   ; Control de Flujo
 L3:
+
+;Operación de suma
 	movl	24(%esp), %edx               ; EDX = a                          ; Transferencia de Datos
 	movl	20(%esp), %eax               ; EAX = b                          ; Transferencia de Datos
 	addl	%edx, %eax                   ; EAX = a + b                      ; Aritmética
@@ -62,6 +74,8 @@ L3:
 	movl	%eax, 4(%esp)                ; Arg1 = a                         ; Transferencia de Datos
 	movl	$LC4, (%esp)                 ; Formato printf suma              ; Transferencia de Datos
 	call	_printf                      ; printf("Suma...")                ; Control de Flujo
+
+;Operación resta
 	movl	24(%esp), %edx               ; EDX = a                          ; Transferencia de Datos
 	movl	20(%esp), %eax               ; EAX = b                          ; Transferencia de Datos
 	subl	%eax, %edx                   ; EDX = a - b                      ; Aritmética
@@ -75,6 +89,8 @@ L3:
 	movl	%eax, 4(%esp)                ; Arg1 = a                         ; Transferencia de Datos
 	movl	$LC5, (%esp)                 ; Formato printf resta             ; Transferencia de Datos
 	call	_printf                      ; printf("Resta...")               ; Control de Flujo
+
+;Operación multiplicación
 	movl	24(%esp), %edx               ; EDX = a                          ; Transferencia de Datos
 	movl	20(%esp), %eax               ; EAX = b                          ; Transferencia de Datos
 	imull	%edx, %eax                   ; EAX = a * b                      ; Aritmética
@@ -87,6 +103,8 @@ L3:
 	movl	%eax, 4(%esp)                ; Arg1 = a                         ; Transferencia de Datos
 	movl	$LC6, (%esp)                 ; Formato printf mult              ; Transferencia de Datos
 	call	_printf                      ; printf("Multiplicacion...")      ; Control de Flujo
+
+;Operación división
 	movl	24(%esp), %eax               ; EAX = a                          ; Transferencia de Datos
 	movl	20(%esp), %ecx               ; ECX = b                          ; Transferencia de Datos
 	cltd                                ; Sign-extend EAX->EDX:EAX          ; Lógica
@@ -103,10 +121,14 @@ L3:
 	movl	24(%esp), %eax               ; EAX = a                          ; Transferencia de Datos
 	subl	$5, %eax                     ; EAX = a - 5                      ; Aritmética
 	movl	%eax, 24(%esp)               ; a = resultado                    ; Transferencia de Datos
+
+;Condición ciclo While
 L2:
 	movl	24(%esp), %eax               ; EAX = a                          ; Transferencia de Datos
 	testl	%eax, %eax                   ; Prueba si a > 0                  ; Lógica
 	jg	L3                             ; Salta al ciclo si a > 0           ; Control de Flujo
+
+;Retorno Main
 	movl	$0, %eax                     ; return 0                         ; Transferencia de Datos
 	leave                              ; Restaura stack                   ; Transferencia de Datos
 	.cfi_restore 5
